c8d79892c32690787e42ac71b9b9a34b301f4402
Michael Lan <michaellan202@gmail.com>
2023-06-25 12:32:47 -0700

    fix: escape HTML apostrophe

--- src/app/about/EganWang.tsx
+++ src/app/about/EganWang.tsx
@@ -8,7 +8,7 @@ const EganWangCard = () => (
     avatar={eganWang}
     tags={[Tag.CurrentInstructor, Tag.AMC8]}
   >
-    Egan is a first-year in UT Austin's honors engineering program.
+    Egan is a first-year in UT Austin&apos;s honors engineering program.
     He enjoys writing in his spare time, but also takes
     pleasure in debate. Egan teaches AMC math but has extensive background in other
     subjects as well such as Java and science.
--- src/app/about/EricLan.tsx
+++ src/app/about/EricLan.tsx
@@ -10,7 +10,7 @@ const EricLanCard = () => (
   >
     Eric is a junior at Irvington High School. He is interested in scripting, modeling, and graphic design.
     In his free time, Eric enjoys hiking, gaming, photography, and pondering life&apos;s big questions.
-    Eric finds solace and inspiration in the great outdoors. Hiking through nature's scenic trails allows
+    Eric finds solace and inspiration in the great outdoors. Hiking through nature&apos;s scenic trails allows
     him to escape the hustle and bustle of everyday life, connecting him with the serenity and beauty of the natural world.
   </Card>
 )
