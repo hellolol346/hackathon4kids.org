99f0ad373765ab8f61927679f3d861e52c99418a
Michael Lan <michaellan202@gmail.com>
2023-12-28 17:04:59 -0800

    feat: Add unformatted directions

---
+++ src/app/code.org/page.tsx
@@ -0,0 +1,34 @@
+const CodeOrg = () => {
+  return (
+    <p>
+      Hello!
+
+      In my class, your student is learning computer science on Code.org, a fun, creative platform for learning computer science and basic coding to create interactive animations, games, or apps. Your interest in what your child is learning is critical, and Code.org makes it easy to stay involved.
+
+      Step 1 - Encourage your child, show interest in computer science
+      One of the best ways is to ask your child to explain what they’re learning and show you a project they are proud of, see details. Or watch one of these videos together.
+
+      Step 2 - Get your child set up to use Code.org at home
+      Our class uses Google Classroom accounts to sign in. To have your student sign in to Code.org at home, do the following:
+
+      Go to https://studio.code.org/ and click 'Sign In'
+
+      Choose 'Continue with Google'
+      Sign in via the Google sign-in dialog
+      At the top of their homepage, your student can continue the course they are doing with their classroom at school. They can also create their own games or artwork in the Project Gallery or check out code.org/athome for ideas for things to work on at home.
+
+      Step 3 - Connect your email to your student's account
+      Keep up to date with what your student is working on and receive updates from Code.org. Have your child sign in to Code.org and then enter your email in Account Settings or click here.
+
+      Why computer science
+      Six different studies show: children who study computer science perform better in other subjects, excel at problem solving, and are 17% more likely to attend college. Computer science teaches students critical thinking, problem solving, and digital citizenship, and benefits all students, no matter what opportunities they pursue in the future. And learning to make interactive animations, code-art, games, and apps on Code.org encourages creativity and makes learning fun.
+
+      Code.org's commitment to student privacy
+      Code.org assigns utmost importance to student safety and security. As a not-for-profit organization, they are transparent about the limited data they collect and how they use it. They do not sell data or exploit it for financial gain. They do not sell ads. Code.org has signed the Student Privacy Pledge and their privacy practices are highly rated by Common Sense Media. You can find further details by viewing Code.org's Privacy Policy.
+
+      Please let me know if you have any questions and thank you for your continued support of your child and of our classroom!
+    </p>
+  )
+}
+
+export default CodeOrg
