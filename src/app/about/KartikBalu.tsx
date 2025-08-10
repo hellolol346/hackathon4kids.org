407b5ae6471e597d658c28c6987091713fbe582d
hellolol346 <39713422+hellolol346@users.noreply.github.com>
2025-08-10 13:08:54 -0700

    Update KartikBalu.tsx

--- src/app/about/KartikBalu.tsx
+++ src/app/about/KartikBalu.tsx
@@ -4,13 +4,13 @@ import kartikbalu from 'public/kartik_balu.jpg'
 const KartikBaluCard = () => (
   <Card
     name="Kartik"
-    subtitle="Director of Marketing & Outreach"
+    subtitle="Co-President"
     avatar={kartikbalu}
     tags={[Tag.CurrentInstructor, Tag.APCSA]}
   >
-    Kartik is a Junior at Cupertino High School. He is interested in Coding,
+    Kartik is a Senior at Cupertino High School. He is interested in Coding,
     Math and Soccer. In his free time, he enjoys hiking and spending time with
-    his family. He also enjoys playing video games with his friends. Being with
+    his family. He also enjoys playing soccer with his friends. Being with
     friends is something he always prefers which helps balance his high school
     academic life.
   </Card>
