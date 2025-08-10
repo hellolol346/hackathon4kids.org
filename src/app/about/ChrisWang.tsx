b281c5e22b17f4be38d48c520ec544b2cd0c0f80
hellolol346 <39713422+hellolol346@users.noreply.github.com>
2025-08-10 13:16:47 -0700

    Update ChrisWang.tsx

--- src/app/about/ChrisWang.tsx
+++ src/app/about/ChrisWang.tsx
@@ -8,7 +8,7 @@ const ChrisWangCard = () => (
     avatar={chrisWang}
     tags={[Tag.RetiredInstructor, Tag.AMC10, Tag.APPhysics1]}
   >
-    Chris is a junior at Mission San Jose High School. He is interested in a
+    Chris is a graduate from Mission San Jose High School. He is interested in a
     variety of STEM related topics such as mathematics, reading, science, and
     especially astronomy. He enjoys witnessing the wonders of the natural world
     and outer space. Chris is also one who is always willing to help out others
