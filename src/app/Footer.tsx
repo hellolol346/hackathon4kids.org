5a8537116904d6375b3cf6fbccde13acce103013
hellolol346 <39713422+hellolol346@users.noreply.github.com>
2025-08-10 13:45:43 -0700

    Update Footer.tsx

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -4,14 +4,14 @@ const Footer = () => {
       <p className="mt-auto text-center text-gray-600 mb-2 mx-5">
         Have questions or comments?
         <br />
-        The fastest way to reach us is to email our advisor
+        The fastest way to reach us is to email us at 
         {' '}
-        <a href="mailto:michaellan202@gmail.com" className="underline text-blue-700">
-          Michael
+        <a href="mailto:hackathon4kidsevent@gmail.com" className="underline text-blue-700">
+          H4K
         </a>.
       </p>
       <p className="mt-auto text-center text-gray-400">
-        &copy; 2023 Hackathon4Kids
+        &copy; 2025 Hackathon4Kids
       </p>
     </div>
   )
