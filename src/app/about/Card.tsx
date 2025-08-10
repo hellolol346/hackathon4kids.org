6732a5e6a869a9a666109ad19d4232403149d1e7
hellolol346 <39713422+hellolol346@users.noreply.github.com>
2025-08-10 13:59:22 -0700

    Update Card.tsx

--- src/app/about/Card.tsx
+++ src/app/about/Card.tsx
@@ -37,8 +37,6 @@ const tagInfo = (tag: Tag) => {
       return ['bg-fuchsia-400', '🤖 Robotics']
     case Tag.APPhysics1:
       return ['bg-teal-400', '⚙️  AP Physics 1']
-    case Tag.IntroToAlgebra:
-      return ['bg-orange-500', '✏️ Intro To Algebra']
   }
 }
 
